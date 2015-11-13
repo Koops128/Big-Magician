4a81408b70fe49c227b12b86d0af64e7f28b7c60
ash <aminoo@uw.edu>
2015-11-13 14:11:08 -0800

    howdy.

--- GreatBigMagicPlan/src/tester.java
+++ GreatBigMagicPlan/src/tester.java
@@ -4,6 +4,8 @@ public class tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(EasterEgg());
+		//ash wrote this
+		System.out.println("BIGO BOY");
 	}
 
 	/**
