43cbc0e82114bd7c3dde6a480e691c0060869cb2
Koops128 <seanevanm@hotmail.com>
2015-11-14 22:53:49 -0800

    added JUnit Test

---
+++ GreatBigMagicPlan/EasterEggTest.java
@@ -0,0 +1,36 @@
+package GUI;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Tests of the AboutFrame class.
+ * 
+ * @author Paul Gray
+ * @version November 2015
+ */
+public class EasterEggTest {
+    
+    // The test fixture. The Objects I will use in the tests.
+    /** An AboutFrame to use in the tests. */
+    private AboutFrame myFrame;
+
+    /**
+     * A method to initialize the test fixture before each test.
+     */
+    @Before
+    public void setUp() {
+        myFrame = new AboutFrame();
+    }
+
+    /**
+     * Test method for... Something?.
+     */
+    @Test
+    public void test() {
+    	
+    	
+    }
+
+}
