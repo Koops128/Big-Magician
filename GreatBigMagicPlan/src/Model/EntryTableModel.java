21395847a18d0b768cc1dbc45a10316a6718d658
Matthew <Matthew@Matt-Desktop.Home>
2015-12-09 00:41:26 -0800

    Added Modify, a method to get the types and a way to get a filtered table to Bank and fixed the delete function, and i think i fixed the database issue

--- GreatBigMagicPlan/src/Model/EntryTableModel.java
+++ GreatBigMagicPlan/src/Model/EntryTableModel.java
@@ -10,93 +10,95 @@ import javax.swing.table.AbstractTableModel;
 
 /**
  * @author Mindy
- *
+ * @author Matthew Cles
  */
 public class EntryTableModel extends AbstractTableModel {
-	
-	public static final String[] COL_NAMES = {"Title", "Type", "Description"};
-	
-	/**
-	 * Awwww yeeeeeeaaaahhhhhhhhhhhhhhhhhhh, yuh gotta get Shwifty
-	 */
-	private static final long serialVersionUID = -8580338694528729389L;
-	String[][] stuff;
-	List<Entry>  myList;
+    
+    public static final String[] COL_NAMES = {"Title", "Type", "Description"};
+    
+    /**
+     * Awwww yeeeeeeaaaahhhhhhhhhhhhhhhhhhh, yuh gotta get Shwifty
+     */
+    private static final long serialVersionUID = -8580338694528729389L;
+    
+    List<Entry>  myList;
+    
+    public EntryTableModel() {
+        myList = new ArrayList<Entry>();
+    }
+    
+    public void add(Entry e) {
+        myList.add(e);
+    }
+    
+    public void remove(Entry e) {
+        myList.remove(e);
+    }
 
-	/**
-	 * 
-	 */
-	public EntryTableModel(String[][] stuff) {
-		this.stuff = stuff;
-	}
-	
-	public EntryTableModel() {
-		myList = new ArrayList<Entry>();
-	}
-	
-	public void add(Entry e) {
-		myList.add(e);
-	}
-	
-	public void remove(Entry e) {
-		myList.remove(e);
-	}
-	
-	public boolean contains(Entry e) {
-		return myList.contains(e);
-	}
-	
-	public boolean contains(String s) {
-		for (Entry e : myList) {
-			if (e.myTitle.equals(s))
-				return true;
-		}
-		return false;
-	}
-	
-	public Entry get(String s) {
-		for (Entry e : myList) {
-			if (e.myTitle.equals(s))
-				return e;
-		}
-		return null;
-	}
+    public void remove(String s) {
+        for (Entry e : myList) {
+            if (e.myTitle.equals(s)) {
+                myList.remove(e);
+                return;
+            }
+        }
+    }
+    
+    public boolean contains(Entry e) {
+        return myList.contains(e);
+    }
+    
+    public boolean contains(String s) {
+        for (Entry e : myList) {
+            if (e.myTitle.equals(s))
+                return true;
+        }
+        return false;
+    }
+    
+    public Entry get(String s) {
+        for (Entry e : myList) {
+            if (e.myTitle.equals(s))
+                return e;
+        }
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see javax.swing.table.TableModel#getColumnCount()
-	 */
-	@Override
-	public int getColumnCount() {
-		return 3;
-	}
-	
+    /* (non-Javadoc)
+     * @see javax.swing.table.TableModel#getColumnCount()
+     */
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+    
 
-	
-	public String getColumnName(int x) {
-		return COL_NAMES[x];
-	}
+    
+    public String getColumnName(int x) {
+        return COL_NAMES[x];
+    }
 
-	/* (non-Javadoc)
-	 * @see javax.swing.table.TableModel#getRowCount()
-	 */
-	@Override
-	public int getRowCount() {
-		return myList.size();
-	}
+    /* (non-Javadoc)
+     * @see javax.swing.table.TableModel#getRowCount()
+     */
+    @Override
+    public int getRowCount() {
+        return myList.size();
+    }
 
-	/* (non-Javadoc)
-	 * @see javax.swing.table.TableModel#getValueAt(int, int)
-	 */
-	@Override
-	public Object getValueAt(int x, int y) {
-		Entry e = myList.get(x);
-		if (y == 0) {
-			return e.myTitle;
-		} else if (y == 1) {
-			return e.myType;
-		} else {
-			return e.myDescription;
-		}
-	}
+    /* (non-Javadoc)
+     * @see javax.swing.table.TableModel#getValueAt(int, int)
+     */
+    @Override
+    public Object getValueAt(int x, int y) {
+        Entry e = myList.get(x);
+        if (y == 0) {
+            return e.myTitle;
+        } else if (y == 1) {
+            return e.myType;
+        } else {
+            return e.myDescription;
+        }
+    }
 
 }
