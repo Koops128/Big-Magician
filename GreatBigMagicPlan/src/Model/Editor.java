09798b53c9a7c3ec5f1e010b6810282851a634ec
Paul Gray <Paulgray215@gmail.com>
2015-12-11 14:11:24 -0800

    Added javadocs and some better code

--- GreatBigMagicPlan/src/Model/Editor.java
+++ GreatBigMagicPlan/src/Model/Editor.java
@@ -3,28 +3,44 @@ package Model;
 import javax.swing.table.TableModel;
 
 /**
- * DIS EDITR B GAWD CALSS
+ * This Editor connects functionality of the database to the GUI
  * @author Paul Gray(Captain EagleFort)
+ * @version 10.45.3.13a
  */
 public class Editor {
-	
+	/** An instance of the language library.*/
 	private Bank myBank = new Bank();
-	private Entry myCurrentEntry = null;
-	private boolean editing = false;
-	private boolean canEdit = false;
 	
+	/** An instance of an entry, set to be the currently selected entry.*/
+	private Entry myCurrentEntry = null;
+	
+	/**
+	 * This method adds the newly constructed entry to the language library.
+	 * @param theEntry The Entry object you want to add to the library.
+	 */
 	public void add(Entry theEntry) {
 		myBank.addEntry(theEntry);
 	}
+	
+	/**
+	 * This method removes the currently selected entry from the language library.
+	 * @param theEntry The Entry object to be deleted from the library.
+	 */
 	public void remove(Entry theEntry){
-		myBank.deleteEntry(theEntry);
-		
+		myBank.deleteEntry(theEntry);		
 	}
-	public Entry getCurrentEntry() throws NullPointerException {
-		if (myCurrentEntry == null) throw new NullPointerException();
+	/**
+	 * This method returns the currently selected Entry object.
+	 * @return Entry The selected entry.
+	 */
+	public Entry getCurrentEntry() {
 		return myCurrentEntry;
 	}
 	
+	/**
+	 * This method sets the current Entry to the value passed in.
+	 * @param theEntry The Entry object to be set.
+	 */
 	public void setCurrentEntry(String theEntry) {
 		if (theEntry == null) {
 			myCurrentEntry = null;
@@ -32,22 +48,28 @@ public class Editor {
 		}
 		myCurrentEntry = myBank.getEntry(theEntry);
 	}
-	
-	public void changeEntry(String title, String type, String desc, 
-			String content) throws NullPointerException {
+	/**
+	 * This method changes the values of the currently selected 
+	 * Entry object to the parameters passed to it.
+	 * @param theTitle The new title for the Entry.
+	 * @param theType The new type for the Entry.
+	 * @param theDesc The new description for the Entry.
+	 * @param theContent The new content for the Entry.
+	 * @throws NullPointerException
+	 */
+	public void changeEntry(String theTitle, String theType, String theDesc, 
+			String theContent) throws NullPointerException {		
+		
 		if(myCurrentEntry == null) throw new NullPointerException();
-		myBank.deleteEntry(myCurrentEntry.getTitle());
-		myCurrentEntry.setContent(content);
-		myCurrentEntry.setDescription(desc);
-		myCurrentEntry.setTitle(title);
-		myCurrentEntry.setType(type);
-		myBank.addEntry(myCurrentEntry);
+		Entry theNewEntry = new Entry(theTitle, theType, theDesc, theContent);
+		myBank.modifyEntry(theNewEntry, myCurrentEntry);
+
 	}
-	
+	/**
+	 * This method returns the current table model object.
+	 * @return TableModel the current state of the database.
+	 */
 	public TableModel getTable() {
 		return myBank.getTable();
-	}
-	
-			
-			
+	}			
 }
